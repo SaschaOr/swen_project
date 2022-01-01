eff28191904b7be336e9ec39493eee811ceca93f
SaschaOr <26334597+SaschaOr@users.noreply.github.com>
2022-01-01 16:52:24 +0100

    creating dictionaries

---
+++ MonsterTradingCardGame/MonsterTradingCardGame/EffectivenessDictionaries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame
+{
+    public class EffectivenessDictionaries
+    {
+        private Dictionary<string, string> spellEffectiveness = new Dictionary<string, string>();
+    }
+}
